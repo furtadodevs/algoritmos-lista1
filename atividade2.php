a56ff192524e3f2e4d9ee2adb5730b527247e4e6
Gabrielle Furtado <gabriellerodriguesfurtado@gmail.com>
2026-07-08 19:50:18 -0300

    Atividade 2

---
+++ atividade2.php
@@ -0,0 +1,38 @@
+<?php
+/*Um buffet cobra R$ 85,00 por pessoa para fornecer alimentação durante um
+evento. Crie variáveis para armazenar a quantidade de convidados e calcule o
+valor total do buffet.
+Resultado esperado: 120 convidados a R$ 85,00 precisam gerar um orçamento
+de R$ 10.200,00.*/
+
+// Entrada - variáveis
+$qtdConvidados = 120;
+$valorBuffet = 85.00;
+
+// Processamento
+$orcamento = $qtdConvidados * $valorBuffet;
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h3>Atividade 2</h3>
+    <p>Um buffet cobra R$ 85,00 por pessoa para fornecer alimentação durante um
+        evento. Crie variáveis para armazenar a quantidade de convidados e calcule o
+        valor total do buffet.
+        Resultado esperado: 120 convidados a R$ 85,00 precisam gerar um orçamento
+        de R$ 10.200,00.</p>
+
+    <p> <?php echo ("Orçamento: " . $orcamento); ?> </p>
+</body>
+
+</html>
